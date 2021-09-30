c67650ef9ba359f3c203ea1e558c3a8985b38bee
Leonardo Sousa <leonardofsousa@outlook.com>
2021-09-30 02:42:54 -0300

    Configurando etapas para deploy no Heroku

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/app-heroku'));
+app.use(express.static('./dist/store-admin-panel'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/app-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/store-admin-panel/index.html'}),
 );
 
 // Start the app by listening on the default Heroku port
